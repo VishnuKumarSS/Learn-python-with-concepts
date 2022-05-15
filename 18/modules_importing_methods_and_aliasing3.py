e3a353cd02ee0973a7ef6f9dc0903239f786325b
VishnukumarSS <vishnukumarss941@gmail.com>
2022-05-16 00:12:06 +0530

    Pushed modules_importing_methods_and_aliasing3.py

---
+++ 18/modules_importing_methods_and_aliasing3.py
@@ -0,0 +1,29 @@
+# normal importing method
+# import module_name
+
+# example:
+import turtle
+object_name = turtle.Turtle()
+
+# from method
+# from module_name import class_names,function_names,etc...
+
+# example:
+from turtle import Turtle
+object_name = Turtle()
+
+# * method
+# from module_name import *
+# * means ( all ).
+
+#example:
+from turtle import *
+object_name = Turtle()
+
+# aliasing modules
+# import module_name as any_name
+
+# example:
+import turtle as T
+object_name = T.Turtle()
+

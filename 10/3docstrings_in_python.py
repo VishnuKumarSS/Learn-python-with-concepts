5fa3548afe3fe14d2bbb0c7d59e2f1a3a7aa04e0
VishnukumarSS <vishnukumarss941@gmail.com>
2022-05-28 00:11:48 +0530

    Pushed 3docstrings_in_python.py

---
+++ 10/3docstrings_in_python.py
@@ -0,0 +1,14 @@
+# docstrings are used to describe the working of the function
+# example:
+str() # here str() is a function , when we point our cursor to the paranthesis of the function 
+# it shows some text . which is the docstring
+
+# the first line after defining the function is the docstring.
+
+# how to define our own docstring to the function
+def starz(): # creating function
+    "This function does not take any parameters" # this line is the docstring
+    # we can use triple quotes to define long description.
+starz() # here calling the function, 
+# and when we point our cursor to the paranthesis
+# of the function is show some text which is the docstring which we have created.

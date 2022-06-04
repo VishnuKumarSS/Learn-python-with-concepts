b7811879a607faa726cb37210452dde0a1d7e335
VishnukumarSS <vishnukumarss941@gmail.com>
2022-06-05 00:11:35 +0530

    Pushed 1functions_with_outputs.py

---
+++ 10/1functions_with_outputs.py
@@ -0,0 +1,6 @@
+def format_name(first,last):
+    name=(first+" "+last).title()
+    return name    
+first=input("Enter your first name: ")
+last=input("Enter your last name: ")
+print(format_name(first,last))

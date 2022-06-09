243378d2c3ef95f2e645273b8e5a57bda127157a
VishnukumarSS <vishnukumarss941@gmail.com>
2022-06-10 00:11:25 +0530

    Pushed 3_what_is_instance.py

---
+++ 19/3_what_is_instance.py
@@ -0,0 +1,10 @@
+# What is instances?
+    # all the objects are the instance of the class.
+
+from turtle import Turtle, Screen
+
+tim= Turtle()
+jim= Turtle()
+
+# here, jim and tim are the instances
+# Turtle() is a class.

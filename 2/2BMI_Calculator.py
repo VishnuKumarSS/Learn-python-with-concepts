0472a363e1ef9da2daa49391920ae6dff31c671f
VishnukumarSS <vishnukumarss941@gmail.com>
2022-06-16 00:11:15 +0530

    Pushed 2BMI_Calculator.py

---
+++ 2/2BMI_Calculator.py
@@ -0,0 +1,18 @@
+#Body mass index (BMI) is a value derived from the mass (weight) and height of a person. 
+# The BMI is defined as the body mass divided by the square of the body height, 
+# and is expressed in units of kg/m2, resulting from mass in kilograms and height in metres.
+
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+a=int(weight)
+b=float(height)
+c=a/b**2
+print(int(c))
+
+

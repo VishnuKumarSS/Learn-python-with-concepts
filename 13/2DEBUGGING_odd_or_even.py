eb24cc329944d6f68a04108805eae610bca329e2
VishnukumarSS <vishnukumarss941@gmail.com>
2022-06-20 00:11:07 +0530

    Pushed 2DEBUGGING_odd_or_even.py

---
+++ 13/2DEBUGGING_odd_or_even.py
@@ -0,0 +1,14 @@
+number = int(input("Which number do you want to check?"))
+
+# problem 
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# solution
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  

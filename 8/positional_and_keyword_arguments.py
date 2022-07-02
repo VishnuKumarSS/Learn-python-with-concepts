e1c4201cb4cc28a1193e206b83817496c3c408a5
VishnukumarSS <vishnukumarss941@gmail.com>
2022-07-03 00:10:31 +0530

    Pushed positional_and_keyword_arguments.py

---
+++ 8/positional_and_keyword_arguments.py
@@ -0,0 +1,9 @@
+def my_fun(a,b,c): 
+    print(a,b,c)
+my_fun(2,3,4) # positional arguments
+my_fun(1,2,3) # positional arguments
+
+
+def fun2(a,b,c): 
+    print(a,b,c)
+fun2(b=3,a=1,c=9) # keyword arguments

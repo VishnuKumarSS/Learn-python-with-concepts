6232258e87d5bdf0e2c09c1d82fc1f3453c1ca9f
VishnukumarSS <vishnukumarss941@gmail.com>
2022-07-04 00:10:28 +0530

    Pushed caesar_cipher1.py

---
+++ 8/caesar_cipher1.py
@@ -0,0 +1,53 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# my own function
+# def my_encrypt(text,shift):
+#     cipher_text=""
+#     for i in range(len(text)):
+#         cipher_text+=alphabet[alphabet.index(text[i])+shift]
+#     print("The encoded text is",cipher_text)
+# encrypt(text=text,shift=shift,alphabet=alphabet)
+
+# below function is easy to understand function
+def encrypt(text,shift):
+    cipher_text=""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position+shift 
+        if new_position > 26: # if the position becomes greater than 26
+            new_position=position-26+shift # this will make the position to start again from first
+            # or we can change the list alphabet by copying the values again like this, below
+            """alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']"""
+        new_letter=alphabet[new_position]
+        cipher_text+=new_letter
+    print("The encoded text is",cipher_text)
+encrypt(text=text,shift=shift)
+
+
+
+
+
+
+
+
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.

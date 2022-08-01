c0845c3737ff4029953f5fbd228b424b1dd54189
VishnukumarSS <vishnukumarss941@gmail.com>
2022-08-02 00:09:42 +0530

    Pushed 2looping_through_dictionary.py

---
+++ 9/2looping_through_dictionary.py
@@ -0,0 +1,14 @@
+
+dictionary = {
+    "name1":"starz",
+    "name2":"vishnu",
+    3:"sathish",
+}
+
+# looping through the dictionary
+for i in dictionary:
+    print("key ----",i) 
+    # this returns all the keys in the dictionary
+    # but not returns the value.
+    # to get the value we have to type like the code below
+    print("Value ---",dictionary[i])
